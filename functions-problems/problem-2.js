d490065692112a18bf17502930a49f04837d6148
yanrivera19 <yan.rivera.6@gmail.com>
2021-10-20 13:17:50 -0400

    Changed variables from 'var' to 'let' and 'const'

--- functions-problems/problem-2.js
+++ functions-problems/problem-2.js
@@ -2,24 +2,24 @@
 returns the number of vowels in that string.*/
 
 /*
-step 1: define a JavaScript function that accepts a string as a parameter.
-step 2: lowercase string passed in.
-step 3: split the string passed in, turn it into an array of each individual letter,
-store it in a variable called arrayOfLetters.
-step 4: define a variable called numberOfVowels and set the initial value to be equal to 0.
-step 5: define a variable that defines what a vowel is. an array of vowels.
-step 6: start looping through our array of letters.
-step 7: check if the current letter in our loop is a vowel.
-step 8: increase the numberOfVowels by one for every letter that's a vowel.
-step 9: return the numberOfVowels
+Step 1: Define a JavaScript function that accepts a string as a parameter.
+Step 2: Lowercase string passed in.
+Step 3: Split the string passed in, turn it into an array of each individual letter,
+Store it in a variable called arrayOfLetters.
+Step 4: Define a variable called numberOfVowels and set the initial value to be equal to 0.
+Step 5: Define a variable that defines what a vowel is. an array of vowels.
+Step 6: Start looping through our array of letters.
+Step 7: Check if the current letter in our loop is a vowel.
+Step 8: Increase the numberOfVowels by one for every letter that's a vowel.
+Step 9: Return the numberOfVowels
 */
 
 function vowelsInString(str) { 
   str = str.toLowerCase();
-  var arrayOfLetters = str.split("");
-  var vowels = ["a", "e", "i", "o", "u"]; 
-  var numberOfVowels = 0;
-  for(var i = 0; i < arrayOfLetters.length; i++) {
+  let arrayOfLetters = str.split("");
+  const vowels = ["a", "e", "i", "o", "u"]; 
+  let numberOfVowels = 0;
+  for(let i = 0; i < arrayOfLetters.length; i++) {
     if(vowels.includes(str[i])) {
       numberOfVowels++;
     }
@@ -27,6 +27,6 @@ function vowelsInString(str) {
   return console.log(numberOfVowels);
 }
   
-var sentence = "my name is yan"
+let sentence = "my name is yan"
 
 vowelsInString(sentence);
