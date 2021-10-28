54ef8987f6f0ddc6d28a1e203304c7e69cdb7c93
yanrivera19 <yan.rivera.6@gmail.com>
2021-10-28 14:46:49 -0400

    First commit. Problem completed.

---
+++ array-problems/problem-2.js
@@ -0,0 +1,10 @@
+/*
+Step 1: Define a variable called myColor that has an array of colors as its value.
+Step 2: Define a variable called arrayToString and join the elements of the array into a string with a space
+in between each element, using the join method.
+Step 3: Log into the console the arrayToString variable.
+*/
+
+const myColor = ["Red", "Green", "White", "Black"];
+const arrayToString = myColor.join(" ");
+console.log(arrayToString);
