b0b1db2ca68b82085e2f2e4e63b84901920e5fa5
yanrivera19 <yan.rivera.6@gmail.com>
2021-10-30 13:07:50 -0400

    First commit. Problem completed.

---
+++ object-problems/problem-4.js
@@ -0,0 +1,29 @@
+/*
+Create an object, then write a JavaScript function that checks whether an object contains the specified key.
+
+Step 1: Create an object called cars.
+Step 2: Create a function called isIncluded that takes in an object and
+a key as parameters.
+Step 3: Check if the key that's passed in is included in the object that's passed in. We use 
+the Objects.key method to return the object's properties in an array, and use the includes property
+to check if the key passed in is included in the object passed in.
+Step 4: Return true if the key passed in is included in the object passed in.
+Step 5: Otherwise return false.
+*/
+
+
+let cars = {
+    model: "Corvette",
+    year: "1986",
+    color: "white",
+  };
+
+function isIncluded (obj, key) {
+  if(Object.keys(obj).includes(key)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isIncluded(cars, "year"));
