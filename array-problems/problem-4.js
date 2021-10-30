f401953701270037f986a147c511e00fdb0570d5
yanrivera19 <yan.rivera.6@gmail.com>
2021-10-30 12:26:10 -0400

    Put the program inside a function called noDuplicate

--- array-problems/problem-4.js
+++ array-problems/problem-4.js
@@ -1,19 +1,27 @@
 /*
+Write a JavaScript program to remove duplicate items from an array (consider case sensitivity).
+
 Step 1: Create an array of numbers and store it in a variable called arrayOfNumbers.
-Step 2: Create an empty array and store it in a variable called noDuplicate. This empty 
+Step 2: Create a function called noDuplicate that takes in an array as a parameter
+and returns a new array without duplicated numbers.
+Step 3: Create an empty array and store it in a variable called newArray. This empty 
 array will acquire the numbers in the array that are not repeated.
-Step 3: Loop through the arrayOfNumbers using the forEach method.
-Step 4: Check if the noDuplicate array doesn't include the curent number being looped.
-If it doesn't, then that current number will be added to the array by using the push method. 
-Step 6: Log into the console the noDuplicate array.
+Step 4: Loop through the arrayOfNumbers using the forEach method.
+Step 5: Check if the newArray doesn't include the curent number being looped.
+If it doesn't, then that current number will be added to the array through the use of the push method. 
+Step 6: Return the newArray.
 */
 
 const arrayOfNumbers = [1,2,2,4,5,4,7,8,7,3,6];
-let noDuplicate = [];  
-arrayOfNumbers.forEach( number => {  
-   if(!noDuplicate.includes(number)){  
-      noDuplicate.push(number);  
-   }  
-});  
 
-console.log(noDuplicate);  
+const noDuplicate = array => {
+let newArray = [];  
+arrayOfNumbers.forEach(number => {  
+   if(!newArray.includes(number)){  
+      newArray.push(number);  
+   }  
+});
+  return newArray;
+};
+
+noDuplicate(arrayOfNumbers); 
