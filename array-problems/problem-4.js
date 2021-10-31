bdfac62bd97838c6d833bf85eea372b6ea8c5fa0
yanrivera19 <yan.rivera.6@gmail.com>
2021-10-30 23:35:31 -0400

    Fixed error. I use the forEach method with the array passed in.

--- array-problems/problem-4.js
+++ array-problems/problem-4.js
@@ -6,7 +6,7 @@ Step 2: Create a function called noDuplicate that takes in an array as a paramet
 and returns a new array without duplicated numbers.
 Step 3: Create an empty array and store it in a variable called newArray. This empty 
 array will acquire the numbers in the array that are not repeated.
-Step 4: Loop through the arrayOfNumbers using the forEach method.
+Step 4: Loop through the array passed in using the forEach method.
 Step 5: Check if the newArray doesn't include the curent number being looped.
 If it doesn't, then that current number will be added to the array through the use of the push method. 
 Step 6: Return the newArray.
@@ -16,7 +16,7 @@ const arrayOfNumbers = [1,2,2,4,5,4,7,8,7,3,6];
 
 const noDuplicate = array => {
 let newArray = [];  
-arrayOfNumbers.forEach(number => {  
+array.forEach(number => {  
    if(!newArray.includes(number)){  
       newArray.push(number);  
    }  
