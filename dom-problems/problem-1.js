2da312c251d8d94bcead2fea479872ce403ba892
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-01 08:23:24 -0400

    Changed Step 7 of the comments.

--- dom-problems/problem-1.js
+++ dom-problems/problem-1.js
@@ -7,8 +7,7 @@ Step 2: Connect the HTML file with the JavaScript file with a "script"
 element linking the JavaScript file.
 Step 3: Add a "p" element and give it the id "p1".
 Step 4: On the JavaScript file, create a variable called bgColor and assign 
-to it a program to add a 
-background color to "p1" using the style property.
+to it a program to add a background color to "p1" using the style property.
 */
 
 let bgColor = document.getElementById("p1").style.backgroundColor = "blue";
