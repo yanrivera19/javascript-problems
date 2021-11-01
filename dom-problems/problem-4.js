907c79f922c0fb30b139795ed0e457fa948090d6
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-01 19:17:30 -0400

    Edited comments.

--- dom-problems/problem-4.js
+++ dom-problems/problem-4.js
@@ -8,8 +8,10 @@ Step 3: Write a JS function that displays the number of items and the items in t
 Step 4: Define a variable called dropList to get the "select" element by its Id.
 Step 5: Define a variable called numItems with the length of the dropdown list.
 Step 6: Run a for loop through the options in the dropdown list.
-Step 7: Inside the loop, add an alert-box with the alert method that displays 
-the number of items and the items in the dropdown list.
+Step 7: Add an alert-box with the alert method that displays the number of items and the items 
+in the dropdown list.
+Step 8: Write the function name inside of the onclick event of the button element 
+on the HTML.
 */
 const displayItems = function () {
 	const dropList = document.getElementById("select1"); 
