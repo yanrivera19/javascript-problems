986425341a2196b07c0ec7a92ddd34bbc250c74c
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-01 14:53:56 -0400

    Added comment to step 5.

--- dom-problems/problem-1.js
+++ dom-problems/problem-1.js
@@ -7,7 +7,8 @@ Step 2: Connect the HTML file with the JavaScript file with a "script"
 element linking the JavaScript file.
 Step 3: Add a "p" element and give it the id "p1".
 Step 4: On the JavaScript file, create a variable called bgColor.
-Step 5: In the variable we will first select the element we want to manipulate (p) 
+Step 5: In the variable we will first make reference to the HTML document we're working
+with by using the document object. We then select the element we want to manipulate (p) 
 by its Id name ("p1") using the getElementById method. 
 Step 6: We then use the style property in order to set the CSS style that we want to set 
 to our element. We then use the CSS property backgroundColor and assign it to "blue".
