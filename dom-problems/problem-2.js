f87ae8a072c08ea921adc99279346eaa0fa28043
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-01 13:23:28 -0400

    Added a button element with an onclick event.

--- dom-problems/problem-2.js
+++ dom-problems/problem-2.js
@@ -10,7 +10,7 @@ Step 4: Add an id, href attribute, and text to the anchor element.
 Step 5: Create a JavaScript function called getAttr that gets the HREF attribute of the anchor element. 
 Step 6: Create a variable called "link" to get the href attribute of the anchor element , by using
 the getAttribute property.
-Step 7: Assign the value of the "link "variable to the parent element "p" using the innerHTML property. 
+Step 7: Display the value of the href attribute to its parent element "p" using the innerHTML property. 
 */
 
 const getAttr = function() {
