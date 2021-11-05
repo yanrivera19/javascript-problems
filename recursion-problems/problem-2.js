a4f4441f3db2bb31987f5a98a813d8733556b32e
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-05 11:26:10 -0400

    Added comments.

--- recursion-problems/problem-2.js
+++ recursion-problems/problem-2.js
@@ -9,9 +9,10 @@ Step 4: As long as the stopping condition is not met, we will add the first elem
 array to the the function sumOfInt which will have as its parameter the rest of the array.
 We select the rest of the array applying the slice method to the array passed in and with a 
 parameter of 1, meaning that a copy of the array will be made starting from the second element
-of the array (index of 1) onward. This recursive function will keep calling itself, and the copies
-of the array will keep reducing in length, it will be returning the first element of each copy.
-That will result in the sum of each individual element (integers) of the array.
+of the array (index of 1) onward. This recursive function will keep calling itself with the sliced
+array as its parameter, and consecuentrly the copies of the array will keep reducing in length, and
+it will keep returning the first element of each copy. That will result in the sum of each individual 
+element (integers) of the array.
 */
 
 let arrayOfInt = [1,2,3,4,5,6,7];
